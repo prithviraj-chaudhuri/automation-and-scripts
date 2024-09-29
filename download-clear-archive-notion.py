9159575906df5e6461b4a3f60e7a5c23f0287766
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-09-29 12:08:58 -0400

    Added download archive script

---
+++ download-clear-archive-notion.py
@@ -0,0 +1,35 @@
+from notion_client import Client
+from datetime import datetime
+
+import logging
+import dotenv
+import os
+import os.path
+import json
+
+def get_archived_notion_tasks():
+    notion_client = Client(
+        auth=os.getenv("NOTION_TOKEN"),
+        log_level=logging.INFO
+    )
+    archive_board = notion_client.databases.query(
+        **{
+            "database_id": os.getenv("ARCHIVE")
+        }
+    )
+    tasks = archive_board["results"]
+    print("Retrieved " + str(len(tasks)) + " archived tasks")
+    return tasks, notion_client
+
+def delete_tasks(tasks, notion_client):
+    
+
+if __name__ == '__main__':
+    dotenv.load_dotenv("config/.env")
+    tasks = get_archived_notion_tasks()
+    archive_location = os.getenv("ARCHIVE_LOCATION")
+    date_today = datetime.today().strftime('%Y-%m-%d')
+    filename = archive_location + '/' + date_today + '.json'
+    with open(filename, 'w') as f:
+        json.dump(tasks, f)
+    

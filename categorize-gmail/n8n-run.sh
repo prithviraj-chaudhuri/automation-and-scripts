4f2602991ab8fd11f5149cb66d5f908622d56d7e
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-10-30 21:13:20 -0400

    Using virtual environments and folder

--- categorize-gmail/n8n-run.sh
+++ categorize-gmail/n8n-run.sh
@@ -1,5 +1,9 @@
+mkdir -p gmail && cd gmail
 wget https://raw.githubusercontent.com/prithviraj-chaudhuri/automation-and-scripts/refs/heads/main/categorize-gmail/download-batch-emails.py -O download-batch-emails.py
 wget https://raw.githubusercontent.com/prithviraj-chaudhuri/automation-and-scripts/refs/heads/main/requirements.txt -O requirements.txt
+python -m venv venv
+./venv/bin/activate
 pip install -r requirements.txt
 mkdir -p /saved_data/gmail-categorize
-python3 download-batch-emails.py --config=/secrets --data=/saved_data/gmail-categorize --pages_to_process=5 --update_status_count=20
+python download-batch-emails.py --config=/secrets --data=/saved_data/gmail-categorize --pages_to_process=5 --update_status_count=20
+cd .. && rm -rf gmail

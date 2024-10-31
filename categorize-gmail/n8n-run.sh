24399f0a0c7a920b68b8ba8860be1670617fffb1
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-10-30 21:04:51 -0400

    added bash file to run

---
+++ categorize-gmail/n8n-run.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/prithviraj-chaudhuri/automation-and-scripts/refs/heads/main/categorize-gmail/download-batch-emails.py
+wget https://raw.githubusercontent.com/prithviraj-chaudhuri/automation-and-scripts/refs/heads/main/requirements.txt
+pip install -r requirements.txt
+mkdir -p /saved_data/gmail-categorize
+python3 download-batch-emails.py --config=/secrets --data=/saved_data/gmail-categorize --pages_to_process=5 --update_status_count=20

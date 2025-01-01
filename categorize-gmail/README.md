41c08ac26a9e9917a7089c08e39208e3b98b960e
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2025-01-01 14:18:39 -0500

    Changed logic to classes

--- categorize-gmail/README.md
+++ categorize-gmail/README.md
@@ -1,1 +1,3 @@
 `python download-batch-emails.py --config=../config --data=data --pages_to_process=30 --update_status_count=10 --start_date=2024-11-10 --end_date=2024-11-11`
+
+Earliest 2014-03-01
--- categorize-gmail/scripts/download-batch-emails.py
+++ categorize-gmail/scripts/download-batch-emails.py
@@ -11,79 +11,163 @@ import datetime
 import json
 import csv
 
-def get_google_service(config_path):
-    SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
-    creds = None
-    token_gmail = config_path + "/token-gmail.json"
-    if os.path.exists(token_gmail):
-        creds = Credentials.from_authorized_user_file(token_gmail, SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                config_path + "/credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-            with open(token_gmail, "w") as token:
-                token.write(creds.to_json())
-    try:
-        service = build("gmail", "v1", credentials=creds)
-        return service
-    except:
-        print("No service")
-        return None
+class Instance:
+    def __init__(self, data, start_date, end_date):
+        self.status_file = data + '/status--'+start_date+'--'+end_date+'.json'
+        self.status = None
 
-def list_messages(service, user_id='me', after_date=None, before_date=None, pages_to_process=-1, next_page_token=None):
-    print("Processing pages")
-    query = ''
-    if after_date:
-        query += f'after:{after_date} '
-    if before_date:
-        query += f'before:{before_date}'
-    
-    if query == '':
-        query = 'is:read'
+    def is_instance_started(self):
+        try:
+            with open(self.status_file) as f:
+                self.status = json.load(f)
+                return self.status.is_completed
+        except:
+            return False
 
-    first_page_token = None
-    messages = []
-    try:
-        if next_page_token:
-            print(next_page_token)
-            response = service.users().messages().list(userId=user_id, q=query.strip(), pageToken=next_page_token).execute()
-            messages = response.get('messages', [])
-        else:
-            response = service.users().messages().list(userId=user_id, q=query.strip()).execute()
-            messages = response.get('messages', [])
-        first_page_token = next_page_token
+    def start_instance(self):
+        self.status = {
+            "next_page": '',
+            "next_message": '',
+            "file": 1,
+            "is_complete": False
+        }
+        with open(self.status_file,'w') as outfile:
+            json.dump(self.status, outfile)
 
-        page_count = 1
-        next_page_token = None
-        while 'nextPageToken' in response:
-            next_page_token = response['nextPageToken']
-            print(next_page_token)
-            if pages_to_process == page_count:
-                break
-            response = service.users().messages().list(userId=user_id, q=query.strip(), pageToken=next_page_token).execute()
-            messages.extend(response.get('messages', []))
-            page_count += 1
+    def update_instance(self, next_page_token, next_message_id, file_count, is_complete):
+        self.status = {
+            "next_page": next_page_token,
+            "next_message": next_message_id,
+            "file": file_count,
+            "is_complete": is_complete
+        }
+        with open(self.status_file, 'w') as outfile:
+            json.dump(self.status, outfile)
+
+class Google:
+    def __init__(self, config_path, instance):
+        self.config_path = config_path
+        self.SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+        self.service = None
+        self.instance = instance
+
+    def get_google_service(self):
+        creds = None
+        token_gmail = self.config_path + "/token-gmail.json"
+        if os.path.exists(token_gmail):
+            creds = Credentials.from_authorized_user_file(token_gmail, self.SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.config_path + "/credentials.json", self.SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+                with open(token_gmail, "w") as token:
+                    token.write(creds.to_json())
+        try:
+            service = build("gmail", "v1", credentials=creds)
+            self.service = service
+        except:
+            print("No service")
+
+    def list_messages(self, user_id='me', after_date=None, before_date=None, pages_to_process=-1, next_page_token=None):
+        print("Processing pages")
+        query = ''
+        if after_date:
+            query += f'after:{after_date} '
+        if before_date:
+            query += f'before:{before_date}'
         
-        print(f"Retrieved message count: {len(messages)}")
-        return messages, first_page_token, next_page_token
-    
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return [], None
+        if query == '':
+            query = 'is:read'
 
-def update_status(data, next_page_token, next_message_id, file_count):
-    print("Saving status")
-    stats = {
-        "next_page": next_page_token,
-        "next_message": next_message_id,
-        "file": file_count
-    }
-    with open(data+"/status.json", "w") as outfile:
-        json.dump(stats, outfile)
+        first_page_token = None
+        messages = []
+        try:
+            if next_page_token:
+                print(next_page_token)
+                response = self.service.users().messages().list(userId=user_id, q=query.strip(), pageToken=next_page_token).execute()
+                messages = self.service.get('messages', [])
+            else:
+                response = self.service.users().messages().list(userId=user_id, q=query.strip()).execute()
+                messages = response.get('messages', [])
+            first_page_token = next_page_token
+
+            page_count = 1
+            next_page_token = None
+            while 'nextPageToken' in response:
+                next_page_token = response['nextPageToken']
+                print(next_page_token)
+                if pages_to_process == page_count:
+                    break
+                response = self.service.users().messages().list(userId=user_id, q=query.strip(), pageToken=next_page_token).execute()
+                messages.extend(response.get('messages', []))
+                page_count += 1
+            
+            print(f"Retrieved message count: {len(messages)}")
+            return messages, first_page_token, next_page_token
+        
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            return [], None
+        
+    def get_messages(self, messages, data_path, first_page_token=None, current_message_id=None, update_status_count=100, file_count=1):
+        begin = False
+        emails = []
+        count = 0
+
+        print("Processing Messages")
+        for message in messages:
+            if current_message_id and message['id'] == current_message_id:
+                begin = True
+            elif not current_message_id:
+                begin = True
+            if not begin:
+                continue
+            current_message_id = message['id']
+            print(current_message_id)
+            email_data = self.get_message('me', message['id'])
+            emails.append(email_data)
+            count += 1
+            if count == update_status_count:
+                self.instance.update_instance(data_path, first_page_token, current_message_id, file_count)
+                count = 0
+
+        return emails, current_message_id
+    
+    def get_message(self, user_id, msg_id):
+        try:
+            message = self.service.users().messages().get(userId=user_id, id=msg_id).execute()
+            payload = message.get('payload', {}).get('body', {}).get('data')
+            if payload:
+                email_content = base64.urlsafe_b64decode(payload.encode('UTF-8')).decode('UTF-8')
+            else:
+                email_content = "No content found."
+            
+            msg = {
+                'id': message['id'],
+                # 'snippet': message['snippet'],s
+                # 'email_content': email_content
+            }
+
+            header = message.get('payload', {}).get('headers', {})
+            for info in header:
+                if info["name"] == "From":
+                    msg["sender"] = info["value"]
+                if info["name"] == "Date":
+                    msg["date"] = info["value"]
+                if info["name"] == "Subject":
+                    msg["subject"] = info["value"]
+                if info["name"] == "To":
+                    msg["receiver"] = info["value"]
+
+            return msg
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            return None
+    
 
 def write_csv(emails, data, file_count):
     print("Writting to file")
@@ -92,61 +176,6 @@ def write_csv(emails, data, file_count):
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(emails)
-
-def get_messages(service, data_path, first_page_token=None, current_message_id=None, update_status_count=100, file_count=1):
-    begin = False
-    emails = []
-    count = 0
-
-    print("Processing Messages")
-    for message in messages:
-        if current_message_id and message['id'] == current_message_id:
-            begin = True
-        elif not current_message_id:
-            begin = True
-        if not begin:
-            continue
-        current_message_id = message['id']
-        print(current_message_id)
-        email_data = get_message(service, 'me', message['id'])
-        emails.append(email_data)
-        count += 1
-        if count == update_status_count:
-            update_status(data_path, first_page_token, current_message_id, file_count)
-            count = 0
-
-    return emails, current_message_id
-
-def get_message(service, user_id, msg_id):
-    try:
-        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
-        payload = message.get('payload', {}).get('body', {}).get('data')
-        if payload:
-            email_content = base64.urlsafe_b64decode(payload.encode('UTF-8')).decode('UTF-8')
-        else:
-            email_content = "No content found."
-        
-        msg = {
-            'id': message['id'],
-            # 'snippet': message['snippet'],s
-            # 'email_content': email_content
-        }
-
-        header = message.get('payload', {}).get('headers', {})
-        for info in header:
-            if info["name"] == "From":
-                msg["sender"] = info["value"]
-            if info["name"] == "Date":
-                msg["date"] = info["value"]
-            if info["name"] == "Subject":
-                msg["subject"] = info["value"]
-            if info["name"] == "To":
-                msg["receiver"] = info["value"]
-
-        return msg
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return None
 
 if __name__ == '__main__':
 
@@ -161,23 +190,24 @@ if __name__ == '__main__':
     parser.add_argument("--message_id", help="Page token to start processing from", default=os.environ.get('MESSAGE_ID'))
     args = parser.parse_args()
 
-    service = get_google_service(args.config)
+    instance = Instance(args.data, args.start_date, args.end_date)
+    if not instance.is_instance_started():
+        instance.start_instance()
+    else:
+        print("Process already runs for dates between ", args.start_date, " ", args.end_date)
+        exit
 
-    status = None
-    try:
-        with open(args.data + '/status.json') as f:
-            status = json.load(f)
-    except:
-        print("Status not available")
-        status = None
-
+    google  = Google(args.config, instance)
+    google.get_google_service()
+    
     first_page_token = None
     next_message_id = None
+
     file_count = 1
-    if status:
-        first_page_token = status['next_page']
-        next_message_id = status['next_message']
-        file_count = int(status['file'])
+    if instance.status:
+        first_page_token = instance.status['next_page']
+        next_message_id = instance.status['next_message']
+        file_count = int(instance.status['file'])
     else:
         first_page_token = args.page_token
         next_message_id = args.message_id
@@ -192,14 +222,14 @@ if __name__ == '__main__':
     else:
         before_date = None
 
-    messages, first_page_token, next_page_token = list_messages(service, 'me', after_date, before_date, int(args.pages_to_process), first_page_token)
-    emails, next_message_id = get_messages(service, args.data, first_page_token, next_message_id, int(args.update_status_count), file_count)
+    messages, first_page_token, next_page_token = google.list_messages('me', after_date, before_date, int(args.pages_to_process), first_page_token)
+    emails, next_message_id = google.get_messages(messages, args.data, first_page_token, next_message_id, int(args.update_status_count), file_count)
 
     if len(emails) > 1:
-        update_status(args.data, next_page_token, None, file_count+1)
+        instance.update_instance(args.data, next_page_token, None, file_count+1)
         write_csv(emails, args.data, file_count)
     else:
-        update_status(args.data, next_page_token, None, file_count)
+        instance.update_instance(args.data, next_page_token, None, file_count)
         
     print(f"Next Page Token {next_page_token}")
     print(f"Next message ID {next_message_id}")
